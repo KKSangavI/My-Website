5e8575a78f729b974cda43e6ce542cb8406e8c73
Sangavi K <sangavi1508@gmail.com>
2025-07-29 10:46:13 +0530

    second commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,38 +1,32 @@
-import React from 'react'
-import{Link} from "react-scroll"
-import "./Header.css"
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import './Header.css';
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className='header__left'>
-            <h1>Develop<span>er</span></h1>
-        </div>
-        <div className='header__right'>
-            <Link to="about"smooth={true} duration={500}>
-            <h4>
-                About Me
-            </h4>
-            </Link>
-             <Link to="skills"smooth={true} duration={500}>
-            <h4>
-                Skills
-            </h4>
-            </Link>
-             <Link to="project"smooth={true} duration={500}>
-            <h4>
-                Projects
-            </h4>
-            </Link>
-             <Link to="contact"smooth={true} duration={500}>
-            <h4>
-                Contact
-            </h4>
-            </Link>
-            
-        </div>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Header
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <h1>Develop<span>er</span></h1>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      {/* Nav Links */}
+      <div className={`header__right ${isOpen ? 'show' : ''}`}>
+        <Link to="about" smooth={true} duration={500}><h4>About Me</h4></Link>
+        <Link to="skills" smooth={true} duration={500}><h4>Skills</h4></Link>
+        <Link to="project" smooth={true} duration={500}><h4>Projects</h4></Link>
+        <Link to="contact" smooth={true} duration={500}><h4>Contact</h4></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Components/TopContent/TopContent.jsx
+++ src/Components/TopContent/TopContent.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-scroll'
 import "./TopContent.css"
+// import DOWNLOAD from "../../assets/SANGAVI_CV.pdf";
 
 export const TopContent = () => {
   return (
     <div className='topContent'>
         <div className='topcontent__container'>
             <h1>Sangavi Kannadhasan</h1>
-            <p>Fontend Developer</p>
-            <a href='https://drive.google.com/file/d/19hgC-7AZSU5xGmQiPID8DlcKh3HSA_xu/view?usp=drivesdk'>
-                <button className='topcontent__downloadbutton'>Download CV</button>
+            <p>Frontend Developer</p>
+            <a href="/SANGAVI_CV.pdf" download>
+              <button className='topcontent__downloadbutton'>Download CV</button>
             </a>
             <Link to ="project" smooth={true} duration={500}>
             <button className='topcontent__workbutton'>MY Work</button>
